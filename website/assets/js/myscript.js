afd46cb38062222d351dded0db3cc4f92821dc66
bhiman <bbaghel123@gmail.com>
2023-02-28 23:18:28 +0530

    dashboard update

--- website/assets/js/myscript.js
+++ website/assets/js/myscript.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){     
-    var timer = setInterval(function() {
-
-        var count = $('span.countdown').html();
-        if (count > 1) {
-        $('span.countdown').html(count - 1);
-        } else {
-        //  $('#form').modal('toggle');
-        clearInterval(timer);
-        }
-    }, 1000);
+$(document).ready(function(){
+    
+           var timer = setInterval(function() {
  
+             var count = $('span.countdown').html();
+             console.log(count);
+             if (count > 1) {
+               $('span.countdown').html(count - 1);
+             } else {
+                //  $('#form').modal('toggle');
+                $('#timerdata').html("Time Up")
+               clearInterval(timer);
+             }
+           }, 1000);
  });
